4353e8a3a2f492b7e86bd65586ae4f4833e85ebf
mathiasoverby <143800439+mathiasoverby@users.noreply.github.com>
2025-03-26 10:26:25 +0100

    Initial commit

---
+++ Common/src/main/java/dk/sdu/cbse/common/services/IPostEntityProcessingService.java
@@ -0,0 +1,9 @@
+package dk.sdu.cbse.common.services;
+
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+
+public interface IPostEntityProcessingService {
+
+    void process(GameData gameData, World world);
+}
