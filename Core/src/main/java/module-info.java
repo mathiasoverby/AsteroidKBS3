6fe2bd4e6f337141f653101ecb19993bb65e85c8
mathiasoverby <143800439+mathiasoverby@users.noreply.github.com>
2025-05-07 14:17:03 +0200

    minimal update

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 
 module Core {
     requires Common;
-    requires CommonBullet;
     requires javafx.graphics;
     opens dk.sdu.cbse.main to javafx.graphics;
     uses dk.sdu.cbse.common.services.IGamePluginService;
