669560e76c9f6909f20791851b04dc3cf8278c6d
mathiasoverby <143800439+mathiasoverby@users.noreply.github.com>
2025-05-29 11:18:46 +0200

    Added missing export to core

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -7,5 +7,6 @@ module Core {
     uses dk.sdu.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.cbse.common.services.IPostEntityProcessingService;
     opens dk.sdu.cbse.main to spring.core;
+    exports dk.sdu.cbse.main;
 
 }
